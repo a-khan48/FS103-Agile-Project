692002263d28dabaee896cc1f0efa193ee570551
Ahmad Khan <ahmad.d.khan40@gmail.com>
2023-06-05 11:22:06 -0400

    Added OrderManagement controller template code

--- Restaurant-Management/src/main/java/org/RMS/controllers/OrderManagement.java
+++ Restaurant-Management/src/main/java/org/RMS/controllers/OrderManagement.java
@@ -1,4 +1,21 @@
 package org.RMS.controllers;
 
+import org.RMS.models.MenuItems;
+import org.RMS.models.Order;
+
+import java.util.List;
+import java.util.Map;
+
 public class OrderManagement {
+    private List<Order> orders;
+
+    public OrderManagement(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public Order placeOrder(Map<MenuItems, Integer> items) {
+        Order order = new Order(items);
+        orders.add(order);
+        return order;
+    }
 }
--- Restaurant-Management/src/main/java/org/RMS/models/User.java
+++ Restaurant-Management/src/main/java/org/RMS/models/User.java
@@ -1,4 +1,14 @@
 package org.RMS.models;
-
 public class User {
+    private String username;
+    private String hashedPassword;
+    private Role role;
+
+    public enum Role {
+        STAFF, MANAGER
+    }
+
+    // Constructors
+
+    // Getters and Setters
 }
