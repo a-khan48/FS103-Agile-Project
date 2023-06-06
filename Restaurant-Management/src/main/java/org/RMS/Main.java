72eee48edf179e5027ca8a860dce0b7b95776eb3
sheryang17 <yangs15000@gmail.com>
2023-06-06 08:36:02 -0400

    Providing updates, did add 2 testing scenarios for 2 classes.

--- Restaurant-Management/src/main/java/org/RMS/Main.java
+++ Restaurant-Management/src/main/java/org/RMS/Main.java
@@ -1,7 +1,17 @@
 package org.RMS;
 
+import org.RMS.controllers.UserManagement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // User logins first
+        UserManagement userManagement = new UserManagement(); // Put this in the main file when it's been created..
+        UserManagement.UserManagementMenu();
+
+
+
+        // Ahmad's Menu Management
+
+
     }
 }
