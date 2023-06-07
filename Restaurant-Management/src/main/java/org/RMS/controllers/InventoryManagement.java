a8ebc39442d5f1b32397fcb12f6b1bccf9d15606
Kelan Blash <73241853+KB-Kodes13@users.noreply.github.com>
2023-06-07 13:19:16 -0400

    Update InventoryManagement.java
    added scanner to inventory management

--- Restaurant-Management/src/main/java/org/RMS/controllers/InventoryManagement.java
+++ Restaurant-Management/src/main/java/org/RMS/controllers/InventoryManagement.java
@@ -2,12 +2,15 @@ package org.RMS.controllers;
 import org.RMS.models.Ingredients;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class InventoryManagement {
+   private final Scanner scanner;
     private List<Ingredients> inventory;
 
     public InventoryManagement() {
         this.inventory = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
     }
 
     public void addIngredient(String name, int count) {
@@ -44,10 +47,64 @@ public class InventoryManagement {
 
     public void alertLowIngredients(int threshold) {
         System.out.println("Low Inventory Alert:");
+        boolean isLowInventory = false; // Flag to track if any ingredient is running low
+
         for (Ingredients ingredient : inventory) {
             if (ingredient.getCount() < threshold) {
                 System.out.println(ingredient.getName() + " is running low. Current quantity: " + ingredient.getCount());
+                isLowInventory = true; // Set the flag to true if any ingredient is running low
             }
         }
+
+        if (!isLowInventory) {
+            System.out.println("Inventory is in good standing. No ingredients are running low.");
+        }
+    }
+    public void displayOptions() {
+        System.out.println("Choose an option:");
+        System.out.println("1. Add ingredient");
+        System.out.println("2. Remove ingredient");
+        System.out.println("3. Check ingredient quantity");
+        System.out.println("4. Alert low ingredients");
+        System.out.println("5. Exit");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        switch (choice) {
+            case 1:
+                System.out.print("Enter ingredient name: ");
+                String addName = scanner.nextLine();
+                System.out.print("Enter ingredient count: ");
+                int addCount = scanner.nextInt();
+                addIngredient(addName, addCount);
+                break;
+            case 2:
+                System.out.print("Enter ingredient name: ");
+                String removeName = scanner.nextLine();
+                System.out.print("Enter quantity to remove: ");
+                int removeQuantity = scanner.nextInt();
+                removeIngredient(removeName, removeQuantity);
+                break;
+            case 3:
+                System.out.print("Enter ingredient name: ");
+                String checkName = scanner.nextLine();
+                checkIngredientQuantity(checkName);
+                break;
+            case 4:
+                System.out.print("Enter threshold for low inventory: ");
+                int threshold = scanner.nextInt();
+                alertLowIngredients(threshold);
+                break;
+            case 5:
+                System.out.println("Exiting...");
+                return;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+                break;
+        }
+
+        /*System.out.println(); // Add a newline for better readability
+        displayOptions(); // Recursively call the method to display options again*/
     }
 }
