3f252c1786e84c43debb347f2378a3b8c102584a
Kelan Blash <73241853+KB-Kodes13@users.noreply.github.com>
2023-06-07 15:10:54 -0400

    Update InventoryManagement.java

--- Restaurant-Management/src/main/java/org/RMS/controllers/InventoryManagement.java
+++ Restaurant-Management/src/main/java/org/RMS/controllers/InventoryManagement.java
@@ -18,6 +18,18 @@ public class InventoryManagement {
         inventory.add(ingredient);
     }
 
+    public void addStockIngredient(String name, int quantity) {
+        for (Ingredients ingredient : inventory) {
+            if (ingredient.getName().equals(name)) {
+                int currentQuantity = ingredient.getCount();
+                    ingredient.count += quantity;
+                    System.out.println(quantity + " " + name + "(s) added to the inventory.");
+                return; // Exit the method after processing the ingredient
+            }
+        }
+        System.out.println("Ingredient " + name + " is not available in the inventory.");
+    }
+
     public void removeIngredient(String name, int quantity) {
         for (Ingredients ingredient : inventory) {
             if (ingredient.getName().equals(name)) {
@@ -34,23 +46,24 @@ public class InventoryManagement {
         System.out.println("Ingredient " + name + " is not available in the inventory.");
     }
 
-    public void checkIngredientQuantity(String name) {
+    public void checkIngredientQuantity() {
         for (Ingredients ingredient : inventory) {
-            if (ingredient.getName().equals(name)) {
+           // if (ingredient.getName().equals(name)) {
                 int quantity = ingredient.getCount();
-                System.out.println("Quantity of " + name + " in the inventory: " + quantity);
-                return; // Exit the method after processing the ingredient
-            }
+
+                System.out.println("Quantity of " + ingredient.getName() + " in the inventory: " + quantity);
+               // return; // Exit the method after processing the ingredient
+           // }
         }
-        System.out.println("Ingredient " + name + " is not available in the inventory.");
+        //System.out.println("Ingredient " + name + " is not available in the inventory.");
     }
 
-    public void alertLowIngredients(int threshold) {
+    public void alertLowIngredients() {
         System.out.println("Low Inventory Alert:");
         boolean isLowInventory = false; // Flag to track if any ingredient is running low
 
         for (Ingredients ingredient : inventory) {
-            if (ingredient.getCount() < threshold) {
+            if (ingredient.getCount() < 100) {
                 System.out.println(ingredient.getName() + " is running low. Current quantity: " + ingredient.getCount());
                 isLowInventory = true; // Set the flag to true if any ingredient is running low
             }
@@ -61,50 +74,72 @@ public class InventoryManagement {
         }
     }
     public void displayOptions() {
-        System.out.println("Choose an option:");
-        System.out.println("1. Add ingredient");
-        System.out.println("2. Remove ingredient");
-        System.out.println("3. Check ingredient quantity");
-        System.out.println("4. Alert low ingredients");
-        System.out.println("5. Exit");
-        System.out.print("Enter your choice: ");
-        int choice = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline character
+        while (true) {
+            System.out.println("Choose an option:");
+            System.out.println("1. Add ingredient");
+            System.out.println("2. Add  ingredient(s) to inventory");
+            System.out.println("3. Remove ingredient");
+            System.out.println("4. Check ingredient quantity");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
 
-        switch (choice) {
-            case 1:
-                System.out.print("Enter ingredient name: ");
-                String addName = scanner.nextLine();
-                System.out.print("Enter ingredient count: ");
-                int addCount = scanner.nextInt();
-                addIngredient(addName, addCount);
-                break;
-            case 2:
-                System.out.print("Enter ingredient name: ");
-                String removeName = scanner.nextLine();
-                System.out.print("Enter quantity to remove: ");
-                int removeQuantity = scanner.nextInt();
-                removeIngredient(removeName, removeQuantity);
-                break;
-            case 3:
-                System.out.print("Enter ingredient name: ");
-                String checkName = scanner.nextLine();
-                checkIngredientQuantity(checkName);
-                break;
-            case 4:
-                System.out.print("Enter threshold for low inventory: ");
-                int threshold = scanner.nextInt();
-                alertLowIngredients(threshold);
-                break;
-            case 5:
-                System.out.println("Exiting...");
-                return;
-            default:
-                System.out.println("Invalid choice. Please try again.");
-                break;
-        }
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ingredient name: ");
+                    String addName = scanner.nextLine();
+                    System.out.print("Enter ingredient count: ");
+                    int addCount = scanner.nextInt();
+                    addIngredient(addName, addCount);
+                    break;
+                case 2:
+                    System.out.print("Enter ingredient name: ");
+                    String addStock = scanner.nextLine();
+                    System.out.print("Enter quantity to add: ");
+                    int addQuantity = scanner.nextInt();
+                    addStockIngredient(addStock, addQuantity);
+                    break;
+                case 3:
+                    System.out.print("Enter ingredient name: ");
+                    String removeName = scanner.nextLine();
+                    System.out.print("Enter quantity to remove: ");
+                    int removeQuantity = scanner.nextInt();
+                    removeIngredient(removeName, removeQuantity);
+                    break;
+                case 4:
+                   // System.out.print("Enter ingredient name: ");
+                   // String checkName = scanner.nextLine();
+                    checkIngredientQuantity();
+                    alertLowIngredients();
+                    break;
 
-        /*System.out.println(); // Add a newline for better readability
+                case 5:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+
+
+
+        /*System.out.println();
         displayOptions(); // Recursively call the method to display options again*/
+        }
+    }
+    public void generateIngredients() {
+        addIngredient("Pizza Dough", 300);
+        addIngredient("Tomato Sauce", 300);
+        addIngredient("Cheese", 500);
+        addIngredient("Pepperoni", 600);
+        addIngredient("Italian Sausage Pieces", 500);
+        addIngredient("Ham Pieces", 500);
+        addIngredient("Seasoned Beef", 500);
+        addIngredient("Roasted Red Peppers", 200);
+        addIngredient("Onions", 200);
+        addIngredient("Mushrooms", 400);
+        addIngredient("Tomato Slices", 450);
+        addIngredient("Pineapple Chucks", 200);
     }
 }
