14151eae9a3f7569eded11335a96cb668af8bea0
sheryang17 <yangs15000@gmail.com>
2023-06-07 11:32:25 -0400

    adding 2 testing cases and slight modifications

---
+++ Restaurant-Management/src/test/java/org/RMS/UserManagementTest.java
@@ -0,0 +1,27 @@
+package org.controllers;
+import org.RMS.models.User;
+import org.RMS.controllers.UserManagement;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserManagementTest {
+    private UserManagement userManagement;
+
+    @Before
+    public void setup() {
+        userManagement = new UserManagement();
+    }
+
+    @Test
+    public void testLoginValidCredentials() {
+        User user = userManagement.login("Staff", "Staff");
+        assertNotNull(user);
+    }
+
+    @Test
+    public void testLoginInvalidCredentials() {
+        User user = userManagement.login("Staff1", "thisisthewrongpassword");
+        assertNull(user);
+    }
+}
