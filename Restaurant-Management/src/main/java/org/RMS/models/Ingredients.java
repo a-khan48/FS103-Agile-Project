0b30fe81132ae41b8707c6d36b7eba3b45b00ee0
Kelan Blash <73241853+KB-Kodes13@users.noreply.github.com>
2023-06-07 12:00:15 -0400

    Update Ingredients.java
    updated Ingredients class

--- Restaurant-Management/src/main/java/org/RMS/models/Ingredients.java
+++ Restaurant-Management/src/main/java/org/RMS/models/Ingredients.java
@@ -5,7 +5,7 @@ import org.RMS.controllers.InventoryManagement;
 
 public class Ingredients {
     private String name;
-    private int count;
+    public int count;
 
     public Ingredients (String name, int count) {
         this.name = name;
@@ -18,5 +18,50 @@ public class Ingredients {
 
     public int getCount() {
         return count;
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the InventoryManagement class
+        InventoryManagement inventoryManagement = new InventoryManagement();
+
+        // Add some ingredients to the inventory
+        inventoryManagement.addIngredient("Pizza Dough", 300);
+        inventoryManagement.addIngredient("Tomato Sauce", 300);
+        inventoryManagement.addIngredient("Cheese", 500);
+        inventoryManagement.addIngredient("Pepperoni", 600);
+        inventoryManagement.addIngredient("Italian Sausage Pieces", 500);
+        inventoryManagement.addIngredient("Ham Pieces", 500);
+        inventoryManagement.addIngredient("Seasoned Beef", 500);
+        inventoryManagement.addIngredient("Roasted Red Peppers", 200);
+        inventoryManagement.addIngredient("Onions", 200);
+        inventoryManagement.addIngredient("Mushrooms", 400);
+        inventoryManagement.addIngredient("Tomato Slices", 450);
+        inventoryManagement.addIngredient("Pineapple Chucks", 200);
+
+
+
+
+
+        // Perform some operations on the inventory
+        inventoryManagement.checkIngredientQuantity("Pizza Dough");
+        inventoryManagement.checkIngredientQuantity("Tomato Sauce");
+        inventoryManagement.checkIngredientQuantity("Cheese");
+        inventoryManagement.checkIngredientQuantity("Pepperoni");
+        inventoryManagement.checkIngredientQuantity("Italian Sausage Pieces");
+        inventoryManagement.checkIngredientQuantity("Ham Pieces");
+        inventoryManagement.checkIngredientQuantity("Seasoned Beef");
+        inventoryManagement.checkIngredientQuantity("Roasted Red Peppers");
+        inventoryManagement.checkIngredientQuantity("Onions");
+        inventoryManagement.checkIngredientQuantity("Mushrooms");
+        inventoryManagement.checkIngredientQuantity("Tomato Slices");
+        inventoryManagement.checkIngredientQuantity("Pineapple Chucks" );
+
+        inventoryManagement.removeIngredient("Pizza Dough", 20);
+        inventoryManagement.checkIngredientQuantity("Pizza Dough");
+        inventoryManagement.removeIngredient("Pepperoni", 591);
+        inventoryManagement.checkIngredientQuantity("Pepperoni");
+
+        // Alert for low inventory
+        inventoryManagement.alertLowIngredients(10);
     }
 }
